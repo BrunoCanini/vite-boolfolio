47829dcda602cf2b4b588b7eb8919ff8c57404df
BrunoCanini <brunocanini1994@gmail.com>
2023-07-17 19:10:16 +0200

    prova

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -10,19 +10,105 @@ import axios from 'axios';
                 loading: false,
                 loadingError: false,
                 projects: [],
+                projectPage: 0,
+                projectTotalPages: 0,
             }
         },
         methods: {
+            getProjectsData(){
+
+            },
+            
             getProjects(){
                 this.loading = true;
+
                 axios.get(this.apiUrl + "projects").then((response)=> {
-                    // console.log(response.data);
                     this.projects = response.data.results.data;
+                    this.projectPage = response.data.results.current_page;
+                    this.projectTotalPages= response.data.results.last_page;
                     this.loading = false;
                 }).catch(err=> {
                     this.loading = false;
                     this.loadingError = "Errore caricamento dati";
                 })
+            },
+
+            nextPage(){
+
+                if(this.projectPage < this.projectTotalPages){
+
+                    let config = {
+                        params: {
+                            page: (this.projectPage + 1)
+                        }
+                    };
+
+                    this.loading = true;
+
+                    axios.get(this.apiUrl + "projects", config).then((response)=> {
+                        this.projects = response.data.results.data;
+                        this.projectPage = response.data.results.current_page;
+                        this.projectTotalPages= response.data.results.last_page;
+                        this.loading = false;
+                    }).catch(err=> {
+                        this.loading = false;
+                        this.loadingError = "Errore caricamento dati";
+                    })
+                } else{
+                    console.error("Non ci sono più pagine")
+                }
+            },
+
+            previusPage(){
+
+                if(this.projectPage > 0){
+
+                    let config = {
+                        params: {
+                            page: (this.projectPage - 1)
+                        }
+                    };
+
+                    this.loading = true;
+
+                    axios.get(this.apiUrl + "projects", config).then((response)=> {
+                        this.projects = response.data.results.data;
+                        this.projectPage = response.data.results.current_page;
+                        this.projectTotalPages= response.data.results.last_page;
+                        this.loading = false;
+                    }).catch(err=> {
+                        this.loading = false;
+                        this.loadingError = "Errore caricamento dati";
+                    })
+                } else{
+                    console.error("Non ci sono più pagine")
+                }
+            },
+
+            page(pageNumber){
+
+                if(pageNumber <= this.projectTotalPages){
+
+                    let config = {
+                        params: {
+                            page: pageNumber
+                        }
+                    };
+
+                    this.loading = true;
+
+                    axios.get(this.apiUrl + "projects", config).then((response)=> {
+                        this.projects = response.data.results.data;
+                        this.projectPage = response.data.results.current_page;
+                        this.projectTotalPages= response.data.results.last_page;
+                        this.loading = false;
+                    }).catch(err=> {
+                        this.loading = false;
+                        this.loadingError = "Errore caricamento dati";
+                    })
+                } else{
+                    console.error("Non ci sono più pagine")
+                }
             }
 
         },
@@ -38,6 +124,7 @@ import axios from 'axios';
     <main>
         <h3 v-if="loading" >Caricamento in corso</h3>
         <h3 v-if="loadingError" >{{loadingError}}</h3>
+        <h4>Page:{{ projectPage }} di {{ projectTotalPages }}</h4>
 
         <div v-for="project in projects">
             <h2>{{ project.title }}</h2>
@@ -49,6 +136,12 @@ import axios from 'axios';
             <p>DESCRIZIONE:{{ project.content }}</p>
 
         </div>
+
+        <a @click="previusPage()">Prev</a>
+
+        <a @click="page(pageNumber)" v-for="pageNumber in projectTotalPages">bottone{{ pageNumber }} </a>
+
+        <a @click=" nextPage()">Next</a>
 
     </main>
 
