6d8feb4abfe4a98eaa24332d4c2aa5ec13ca853f
BrunoCanini <brunocanini1994@gmail.com>
2023-07-20 17:51:46 +0200

    feat: card boostrap

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -7,14 +7,12 @@ import axios from 'axios';
         data() {
             return{
                 apiUrl: "http://127.0.0.1:8000/api/",
+                imageUrl: "http://127.0.0.1:8000/storage/",
                 loading: false,
                 loadingError: false,
                 projects: [],
                 projectPage: 0,
                 projectTotalPages: 0,
-                // navLinks: [
-                //     {route: "single-project", label: "single-project", params: {id:""}},
-                // ]
             }
         },
         methods: {
@@ -132,19 +130,20 @@ import axios from 'axios';
 
         <div class="d-flex m-4">
             <div v-for="project in projects" class="card m-2" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">{{ project.title }}</h5>
-                    <p class="card-text">CATEGORIA: {{ project.category ? project.category.name : "Nessuna categoria" }}</p>
-                    <p class="card-text">TECNOLOGIE:
-                        <span v-if="project.technologys.length" v-for="tech in project.technologys" class="card-text" > {{ tech.name }} </span>
-                        <span v-else class="card-text">Nessuna</span>
-                    </p>
-                    <p class="card-text">DESCRIZIONE:{{ project.content }}</p>
-                    <a :href= "'/projects/'+ project.id">vai</a>
-                    <!-- <router-link :to="{name: project.route, params: project.id}" class="nav-link active" aria-current="page">{{link.label}}</router-link> -->
-                </div>
-            </div> 
-        </div>
+                    <img :src="imageUrl + project.image" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ project.title }}</h5>
+                        <p class="card-text">CATEGORIA: {{ project.category ? project.category.name : "Nessuna categoria" }}</p>
+                        <p class="card-text">TECNOLOGIE:
+                            <span v-if="project.technologys.length" v-for="tech in project.technologys" class="card-text" > {{ tech.name }} </span>
+                            <span v-else class="card-text">Nessuna</span>
+                        </p>
+                        <p class="card-text">DESCRIZIONE:{{ project.content }}</p>
+                        <router-link :to="{ name: 'single-project', params: { id: project.id }}" class="btn btn-primary" >Deteils</router-link>                </div>
+            </div>
+                    <!-- <a :href= "'/projects/'+ project.id">vai</a> -->
+                    <!-- <router-link :to="{ name: 'single-project', params: { id: project.id }}">Home</router-link>                </div> -->
+        </div> 
 
         <div class="mx-4">
             <a @click="previusPage()" class="btn btn-primary m-2">Prev</a>
--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -7,6 +7,7 @@ import axios from 'axios';
             return{
                 loading: false,
                 apiUrl: "http://127.0.0.1:8000/api/",
+                imageUrl: "http://127.0.0.1:8000/storage/",
                 loadingError: false,
                 project: null
             }
@@ -27,6 +28,12 @@ import axios from 'axios';
         },
         mounted() {
             this.getProject(this.$route.params.id);
+        },
+        beforeRouteUpdate (to,from){
+            if(from.name == to.name){
+                let newProjectId = to.params.id;
+                this.getProject(newProjectId)
+            }
         }
     }
 
@@ -35,22 +42,24 @@ import axios from 'axios';
 <template>
 
     <section v-if="project">
-        <div class="d-flex m-4">
-            <div class="card m-2" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">{{ project.title }}</h5>
-                    <p class="card-text">CATEGORIA: {{ project.category ? project.category.name : "Nessuna categoria" }}</p>
-                    <p class="card-text">TECNOLOGIE:
-                        <span v-if="project.technologys.length" v-for="tech in project.technologys" class="card-text" > {{ tech.name }} </span>
-                        <span v-else class="card-text">Nessuna</span>
-                    </p>
-                    <p class="card-text">DESCRIZIONE:{{ project.content }}</p>
-                </div>
-            </div> 
+
+        <div class="card m-3" style="width: 18rem;">
+            <img :src="imageUrl + project.image" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">{{ project.title }}</h5>
+                <p class="card-text">CATEGORIA: {{ project.category ? project.category.name : "Nessuna categoria" }}</p>
+                <p class="card-text">TECNOLOGIE:
+                    <span v-if="project.technologys.length" v-for="tech in project.technologys" class="card-text" > {{ tech.name }} </span>
+                    <span v-else class="card-text">Nessuna</span>
+                 </p>
+                <p class="card-text">DESCRIZIONE:{{ project.content }}</p>
+            </div>
         </div>
+
     </section>
 
 </template>
+
 
 <style scoped>
 
